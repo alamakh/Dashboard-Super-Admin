1c0a7d25f2e8b0f9c513e6688045000f1cc10d86
saidrebai <rebaisaid6@gmail.com>
2023-06-01 16:43:40 +0100

    add date and button répondre

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Admins from "./scenes/Admins";
 import Users from "./scenes/Users";
+import Message from "./scenes/msg";
+
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Calendar from "./scenes/calendar/calendar";
@@ -15,7 +17,6 @@ import PieChart from "./components/PieChart";
 
 function App() {
   const token = localStorage.getItem("token");
-
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
 
@@ -31,11 +32,12 @@ function App() {
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/admins" element={<Admins />} />
               <Route path="/users" element={<Users />} />
+              <Route path="/messages" element={<Message />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/LineChart" element={<LineChart />} />
               <Route path="/BarChart" element={<BarChart />} />
               <Route path="/PieChart" element={<PieChart />} />
-              <Route path="/" exact element={<SignInSuperAdmin />}/>
+              <Route path="/" exact element={<SignInSuperAdmin />} />
             </Routes>
           </main>
         </div>
