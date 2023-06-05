cf2a7c8518f392b31ce86937adf5c69faeec43ac
alamakh <alamakhlouf@outlook.com>
2023-06-05 17:26:46 +0200

    update charts with filter

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from "react";
 import Chart from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 import axios from "axios";
+import "./index.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import enUS from "date-fns/locale/en-US";
+
+registerLocale("en-US", enUS);
+setDefaultLocale("en-US");
 
 const BarChart = () => {
   const [revenue, setRevenue] = useState([]);
+  const [selectedMonth, setSelectedMonth] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -17,20 +26,50 @@ const BarChart = () => {
     fetchData();
   }, []);
 
-  function countRevenueByMonth(revenue) {
-    const countByMonth = revenue.reduce((acc, item) => {
-      const { month, expenses } = item;
-      if (!acc[month]) {
-        acc[month] = 0;
-      }
-      acc[month] += expenses;
-      return acc;
-    }, {});
+  function countRevenueByMonth(revenue, selectedMonth) {
+    console.log("Selected month 1:", selectedMonth);
+    if (selectedMonth) {
+      const filteredRevenue = revenue?.filter(
+        (item) =>
+          item?.month?.toLowerCase() ===
+          selectedMonth
+            ?.toLocaleString("en-US", { month: "long" })
+            ?.toLowerCase()
+      );
+      console.log("Filtered revenue 1:", filteredRevenue);
 
-    return countByMonth;
+      const countByMonth = filteredRevenue?.reduce((acc, item) => {
+        const { month, expenses } = item;
+        if (!acc[month]) {
+          acc[month] = 0;
+        }
+        acc[month] += expenses;
+        return acc;
+      }, {});
+
+      return countByMonth;
+    } else {
+      const countByMonth = revenue?.reduce((acc, item) => {
+        const { month, expenses } = item;
+        if (!acc[month]) {
+          acc[month] = 0;
+        }
+        acc[month] += expenses;
+        return acc;
+      }, {});
+
+      return countByMonth;
+    }
   }
 
-  const revenusCounts = countRevenueByMonth(revenue);
+  const handleMonthChange = (date) => {
+    const formattedMonth = date?.toLocaleString("en-US", { month: "long" });
+    setSelectedMonth(date);
+    console.log("Selected month 0 :", formattedMonth);
+  };
+
+  const revenusCounts = countRevenueByMonth(revenue, selectedMonth);
+  console.log("🚀 ", selectedMonth);
   console.log("Revenue by month:", revenusCounts);
   const labels = Object.keys(revenusCounts);
   const dataValues = Object.values(revenusCounts);
@@ -42,14 +81,33 @@ const BarChart = () => {
         label: "Revenue by month",
         backgroundColor: "rgb(255, 75, 1)",
         borderColor: "rgb(254, 152, 1)",
-        // borderWidth: 2,
-        // borderRadius : 7,
         data: dataValues,
       },
     ],
   };
+
+  const clearMonthSelection = () => {
+    setSelectedMonth(null);
+  };
+
+  console.log("Chart data:", data);
+
   return (
-    <div>
+    <div className="Bar_chart">
+      <div className="buttonss">
+        <DatePicker
+          className="month_selector"
+          selected={selectedMonth}
+          onChange={handleMonthChange}
+          showMonthYearPicker
+          dateFormat="MMMM"
+          placeholderText="Select month"
+          locale="en-US"
+        />
+        <button className="clear_butt" onClick={clearMonthSelection}>
+          Clear
+        </button>
+      </div>
       <Bar data={data} />
     </div>
   );
