f4435c072f2de7820b768a739d913c2a9dab4397
Haroun Bouchamia <harounbouchamia123@gmail.com>
2023-12-16 14:33:17 +0100

    here is the new change

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,13 +1,11 @@
-import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import TrafficIcon from "@mui/icons-material/Traffic";
 import Header from "../../components/Header";
 import StatBox from "../../components/StatBox";
-import { useState,useEffect } from "react";
-// import ProgressCircle from "../../components/ProgressCircle";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import BarChart from "../../components/BarChart";
 import LineChart from "../../components/LineChart";
@@ -16,16 +14,12 @@ const Dashboard = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
-  const [pageviews,setPageviews] = useState();
-  const [visits,setVisits] = useState();
+  const [pageviews, setPageviews] = useState();
+  const [visits, setVisits] = useState();
   const [adminCount, setAdminCount] = useState(0);
   const [userCount, setUserCount] = useState(0);
-  
 
-  
   function updateCounter() {
-    
-  
     fetch('http://localhost:8080/api')
       .then(res => res.json())
       .then(data => {
@@ -33,20 +27,20 @@ const Dashboard = () => {
         setVisits(data?.visits);
         sessionStorage.setItem('visit', data?.visits);
         sessionStorage.setItem('pageviews', data?.pageviews);
-        console.log("data",data);
+        console.log("data", data);
       });
   }
 
   /* This haroun talking from my branch 'haroun' hope you are doing well all of you and thanks */
-  
+
   useEffect(() => {
     updateCounter();
-  
+
     // Fetch data every 10 seconds
     const intervalId = setInterval(() => {
       updateCounter();
     }, 1000000);
-  
+
     // Clear interval on unmount
     return () => clearInterval(intervalId);
   }, []);
@@ -71,13 +65,11 @@ const Dashboard = () => {
     fetchData();
   }, []);
 
-
   return (
     <Box m="20px">
       {/* HEADER */}
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
-
       </Box>
 
       {/* GRID & CHARTS */}
@@ -88,13 +80,7 @@ const Dashboard = () => {
         gap="20px"
       >
         {/* ROW 1 */}
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
+        <Box gridColumn="span 4" backgroundColor={colors.primary[400]}>
           <StatBox
             title={adminCount}
             subtitle="Admins"
@@ -102,18 +88,12 @@ const Dashboard = () => {
             increase="+14%"
             icon={
               <PersonAddIcon
-              sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-            />
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
             }
           />
         </Box>
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
+        <Box gridColumn="span 4" backgroundColor={colors.primary[400]}>
           <StatBox
             title={userCount}
             subtitle="Candidats"
@@ -126,13 +106,7 @@ const Dashboard = () => {
             }
           />
         </Box>
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
+        <Box gridColumn="span 4" backgroundColor={colors.primary[400]}>
           <StatBox
             title={pageviews}
             subtitle="Views"
@@ -145,87 +119,26 @@ const Dashboard = () => {
             }
           />
         </Box>
- 
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title={visits}
-            subtitle="Traffic Received"
-            progress="0.80"
-            increase="+43%"
-            icon={
-              <TrafficIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-
-
 
         {/* ROW 2 */}
-        <Box
-           gridColumn="span 7"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-           height ="460px"
-          style={{paddingTop:"20px",paddingBottom:"20px"}}
-        >
-          <Box
-            // mt="25px"
-            p="0 30px"
-            display="flex "
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Box>
-              <Typography
-                variant="h5"
-                fontWeight="600"
-                color={colors.greenAccent[500]}
-              >
-                Revenue Generated
-              </Typography>
-            </Box>
-            <Box>
-              <IconButton>
-                <DownloadOutlinedIcon
-                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
-                />
-              </IconButton>
-            </Box>
-          </Box>
-          <Box width={"100%"} m="-20px 0 0 0">
-            <LineChart isDashboard={true} />
-          </Box>
+        <Box gridColumn="span 8" gridRow="span 2" backgroundColor={colors.primary[400]}>
+          <LineChart isDashboard={true} />
         </Box>
 
-
         {/* ROW 3 */}
-              <Box
-          gridColumn="span 5"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          height="400px"
-        >
+        <Box gridColumn="span 4" backgroundColor={colors.primary[400]}>
           <Typography
             variant="h5"
             fontWeight="600"
             color={colors.greenAccent[500]}
-            sx={{ padding: "30px 30px 0 30px",marginBottom:"20px" }}
+            sx={{ padding: "30px 30px 0 30px", marginBottom: "20px" }}
           >
             Revenue By Month
           </Typography>
           <Box height="250px" mt="-20px">
-            <BarChart className="pie"isDashboard={true} />
+            <BarChart className="pie" isDashboard={true} />
           </Box>
         </Box>
-
       </Box>
     </Box>
   );
